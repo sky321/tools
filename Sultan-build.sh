0308c2d394969705b1f420dab4a9d8639cc5c0a6
sky321 <github.10.csw@spamgourmet.com>
2018-01-20 12:32:05 +0100

    unpatcher include

--- Sultan-build.sh
+++ Sultan-build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # Build LAOS from Sultan Recources for Oneplus 3/3T
+./patcher/unpatcher.sh
 repo sync -c -j 16
-source build/envsetup.sh
 ./patcher/patcher.sh
+source build/envsetup.sh
 breakfast oneplus3
+pause
 brunch oneplus3
 
