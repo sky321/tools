ba48fa8df42393ba7dbae2cdbf60a02e09b9c5d8
sky321 <github.10.csw@spamgourmet.com>
2018-05-19 22:56:06 +0200

    better comment

--- Sultan-build.sh
+++ Sultan-build.sh
@@ -1,12 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 # Build LAOS from Sultan Recources for Oneplus 3/3T
+cd /home/build/android/
+pwd
 ./patcher/unpatcher.sh
+read -p "unPatcher OK?"
 repo sync -c -j 16
+read -p "Sync OK?"
 ./patcher/patcher.sh
+read -p "Patcher OK?"
 source build/envsetup.sh
+read -p "Env OK?"
 mka clean
+read -p "Clean OK?"
 breakfast oneplus3
-pause
+read -p "Breakfast OK?"
 brunch oneplus3
 
