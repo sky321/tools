dee24f619e531850733df8d35f35b7f2c99adde0
sky321 <github.10.csw@spamgourmet.com>
2019-03-14 15:06:10 +0100

    sign keys sh

--- build.sh
+++ build.sh
@@ -3,17 +3,10 @@
 # Build LAOS for Oneplus 3/3T
 cd /home/build/android/
 pwd
-#./patcher/unpatcher.sh
-#read -p "unPatcher OK?"
 repo sync -c -j 16
 read -p "Sync OK?"
-#./patcher/patcher.sh
-#read -p "Patcher OK?"
 source build/envsetup.sh
-#read -p "Env OK?"
 mka clean
-#read -p "Clean OK?"
 breakfast oneplus3
-#read -p "Breakfast OK?"
 brunch oneplus3
 
--- sign-img.sh
+++ sign-img.sh
@@ -6,12 +6,7 @@ mka target-files-package otatools
 
 # sign all the APKs
 croot
-./build/tools/releasetools/sign_target_files_apks -o -d ~/.android-certs \
-    $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip \
-    signed-target_files.zip
+./build/tools/releasetools/sign_target_files_apks -o -d ~/.android-certs $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip signed-target_files.zip
 	
 # generate the installable zip
-./build/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey \
-    --block --backup=true \
-    signed-target_files.zip \
-    signed-ota_update.zip
+./build/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey --block --backup=true signed-target_files.zip signed-ota_update.zip
