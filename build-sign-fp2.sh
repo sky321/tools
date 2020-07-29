2af52ddfd4ab59713ee4e647a603ad521ea76b71
sky321 <github.10.csw@spamgourmet.com>
2020-07-29 07:39:12 +0200

    add FP2

---
+++ build-sign-fp2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Build LAOS for FP2
+START=$(date)
+SECONDS=0
+
+cd /home/build/android/
+pwd
+
+repo sync -c -j 16
+[ $? -ne 0 ] && { echo "Error at repo sync"; exit 1; }
+
+source build/envsetup.sh
+mka clean
+[ $? -ne 0 ] && { echo "Error at make clean"; exit 1; }
+
+breakfast FP2
+
+KEYS_DIR=/home/build/android/.android-certs
+OUT=/home/build/android/out/target/product/FP2
+TARGET_FILES_SIGNED=lineage-$(get_build_var LINEAGE_VERSION)-signed-target_files.zip
+OTA_PACKAGE=lineage-$(get_build_var LINEAGE_VERSION)-signed.zip
+
+# gen target files (only if not run direct after build)
+mka target-files-package otatools
+[ $? -ne 0 ] && { echo "Error at make target files"; exit 1; }
+
+# sign all the APKs
+croot
+./build/tools/releasetools/sign_target_files_apks -o -d $KEYS_DIR $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip $TARGET_FILES_SIGNED
+[ $? -ne 0 ] && { echo "Error at sign APKs"; exit 1; }
+
+# generate the signed installable OTA zip
+./build/tools/releasetools/ota_from_target_files -k $KEYS_DIR/releasekey --block --backup=true $TARGET_FILES_SIGNED $OTA_PACKAGE
+
+END=$(date)
+elapsedseconds=$SECONDS
+echo "Start: $START"
+echo "Ende : $END"
+echo "Duration:"
+TZ=UTC0 printf '%(%H:%M:%S)T\n' "$elapsedseconds"
