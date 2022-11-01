8bfe851180f7f48de4b271a54efb963706de6bf5
skyPad <ab@cd.de>
2022-11-01 21:16:41 +0100

    no error msg at keygen

--- keygen.sh
+++ keygen.sh
@@ -7,7 +7,7 @@ subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/ema
 # keys generate
 mkdir $KEYS_DIR
 for x in releasekey platform shared media networkstack testkey; do ./development/tools/make_key $KEYS_DIR/$x "$subject"; done
-[ $? -ne 0 ] && { echo "Error at make keys"; exit 1; }
+#[ $? -ne 0 ] && { echo "Error at make keys"; exit 1; }
 
 # use new keys in common.mk for vendor (dev-keys)
 #sed -i "1s;^;PRODUCT_DEFAULT_DEV_CERTIFICATE := $KEYS_DIR/releasekey\nPRODUCT_OTA_PUBLIC_KEYS := $KEYS_DIR/releasekey\nPRODUCT_EXTRA_RECOVERY_KEYS := $KEYS_DIR/releasekey\n\n;" "vendor/$vendor/config/common.mk"
